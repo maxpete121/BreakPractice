a086ed62152bf6491d2f4455ddc7947e0512c1f5
Max Petersen <maxpete121@gmail.com>
2023-11-21 15:16:49 -0700

    Functions added

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+
+
+function theKing() {
+    document.getElementById("pc-two").style.display = "none";
+    document.getElementById("pc-three").style.display = "none";
+    document.getElementById("choose-pc").style.display = "none";
+    let comOne = document.getElementById("pc-one");
+    if(comOne = "pc-one") {
+        document.getElementById("pc-one").style.display = "block";
+    }
+
+}
+
+function theQueen() {
+    document.getElementById("pc-one").style.display = "none";
+    document.getElementById("pc-three").style.display = "none";
+    document.getElementById("choose-pc").style.display = "none";
+    let comOne = document.getElementById("pc-Two");
+    if(comOne = "pc-two") {
+        document.getElementById("pc-two").style.display = "block";
+    }
+
+}
+
+function theEmperor() {
+    document.getElementById("pc-one").style.display = "none";
+    document.getElementById("pc-two").style.display = "none";
+    document.getElementById("choose-pc").style.display = "none";
+    let comOne = document.getElementById("pc-three");
+    if(comOne = "pc-three") {
+        document.getElementById("pc-three").style.display = "block";
+    }
+
+}
